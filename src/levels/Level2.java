3026c7f711a323b2193a830d18588869224a1c70
Taieb-belhadjali <Taiebamine.Belhadjali@esprit.tn>
2024-12-09 23:01:27 +0100

    level2

--- src/levels/Level2.java
+++ src/levels/Level2.java
@@ -1,5 +1,6 @@
 package levels;
 
+import models.Subject;
 import models.Teacher;
 import utils.Data;
 
@@ -11,34 +12,34 @@ public class Level2 {
         List<Teacher> teachers = Data.employees();
 
         /* TO DO 1: Retourner le nombre des enseignants dont le nom commence avec s */
-        long nbr = teachers.stream()./*TO DO 1 */
+        long nbr = teachers.stream().filter(teacher -> teacher.getName().toLowerCase().startsWith("s")).count();
 
         /* TO DO 2: Retourner la somme des salaires de tous les enseignants Flutter (hint: mapToInt) */
-        long sum = teachers.stream()./* TO DO 2 */;
+        long sum = teachers.stream().filter(teacher ->teacher.getSubject()== Subject.FLUTTER).mapToInt(Teacher::getSalary).sum();
 
         /* TO DO 3: Retourner la moyenne des salaires des enseignants dont le nom commence avec a */
-        double average = teachers.stream()./* TO DO 3 */;
+        double average = teachers.stream().filter(teacher -> teacher.getName().toLowerCase().startsWith("a")).mapToInt(Teacher::getSalary).average().orElse(0);
 
 
         /* TO DO 4: Retourner la liste des enseignants dont le nom commence par f */
-        List<Teacher> teachers1 = teachers.stream()./* TO DO 4 */;
+        List<Teacher> teachers1 = teachers.stream().filter(teacher -> teacher.getName().toLowerCase().startsWith("f")).toList();
 
         /* TO DO 5: Retourner la liste des enseignants dont le nom commence par s */
-        List<Teacher> teachers2 = teachers.stream()./* TO DO 5 */;
+        List<Teacher> teachers2 = teachers.stream().filter(teacher -> teacher.getName().toLowerCase().startsWith("s")).toList();
 
 
         /* TO DO 5: Retourner true si il y a au min un enseignants dont le salaire > 100000, false si non */
-        boolean test = teachers.stream()./* TO DO 6 */;
+        boolean test = teachers.stream().anyMatch(teacher -> teacher.getSalary() > 100000);
 
         /* TO DO 6: Afficher le premier enseignant Unity le nom commence avec g avec 2 manières différentes */
         /*First way*/
-        teachers.stream()./* TO DO 7 */;
+        teachers.stream().filter(teacher -> teacher.getSubject()== Subject.UNITY).filter(teacher -> teacher.getName().toLowerCase().startsWith("g")).findFirst().ifPresent(System.out::println);
         ;
         /*Second way*/
-        teachers.stream()./* TO DO 7 */;
+        teachers.stream().filter(teacher -> teacher.getSubject()== Subject.UNITY).filter(teacher -> teacher.getName().toLowerCase().startsWith("g")).findAny().ifPresent(System.out::println);
 
         /* TO DO 7: Afficher le deuxième enseignant dont le nom commence avec s */
-        teachers.stream()./* TO DO 8 */;
+        teachers.stream().filter(teacher -> teacher.getName().toLowerCase().startsWith("s")).skip(1).findFirst().ifPresent(System.out::println);
 
     }
 }
